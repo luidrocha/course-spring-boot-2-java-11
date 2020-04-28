98086e7f5f9629ece62a6ce1f9fa2b65cd0a3d88
LuidRocha <luidrocha@yahoo.com.br>
2020-04-28 19:40:58 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/wpcursos/course/entities/User.java
+++ src/main/java/com/wpcursos/course/entities/User.java
@@ -1,90 +1,95 @@
 package com.wpcursos.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable  {
-	
+@Table (name="tb_user") // Da um nome pra tabela a ser criada
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy="client") // Como esta mapeado o atributo do outro lado, em Order
+	
+	private List<Order> orders = new ArrayList<>();
 	
 	
+
 	public User() {
-		
+
 	}
 
-
 	public User(Long id, String name, String email, String phone, String password) {
-		
+
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 
 	public String getPhone() {
 		return phone;
 	}
 
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+		
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	
 
 	@Override
 	public int hashCode() {
@@ -93,7 +98,6 @@ public class User implements Serializable  {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -112,16 +116,10 @@ public class User implements Serializable  {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "user [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
 				+ "]";
 	}
-	
-	
-	
-	
-	
 
 }
