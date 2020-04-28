b82a47c8d8ddf1735811aad2cba221af670b74a9
LuidRocha <luidrocha@yahoo.com.br>
2020-04-28 17:29:59 -0300

    Service layer, component registration create

--- src/main/java/com/wpcursos/course/resources/UserResource.java
+++ src/main/java/com/wpcursos/course/resources/UserResource.java
@@ -1,29 +1,48 @@
 package com.wpcursos.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wpcursos.course.entities.User;
+import com.wpcursos.course.services.UserService;
 
 // Classe que disponibiliza os recursos WEB da classe USER
 // faz parte da camada de Controladores REST
 
 @RestController
-@RequestMapping (value = "/users")
+@RequestMapping(value = "/users")
 @ResponseBody
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
-		
-		User user = new User(10L, "Jose luis","luidrocha@gmail.com","21-99254-2563","Brasil2020");
-		
-		return ResponseEntity.ok().body(user);
+	@Autowired
+	private UserService service;
 
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+
+	}
+
+	// Permite passa o parametro para requisição, nesse caso o id.
+	
+	@GetMapping (value ="/{id}")
+	public ResponseEntity<User>	findById(@PathVariable long id){ // Essa anotação@PathVariable informa que o metodo vai receberum parametro por requisição
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+		
 		
 	}
+
 }
---
+++ src/main/java/com/wpcursos/course/services/UserService.java
@@ -0,0 +1,36 @@
+package com.wpcursos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wpcursos.course.entities.User;
+import com.wpcursos.course.repositories.UserRepository;
+
+// REgistra classe como component
+
+// @Repository pra registrar classe de  Repository, @Service pra registrar Serviços
+
+@Service
+public class UserService {
+
+	// Gerencia a dependencia automaticamente
+	@Autowired
+	private UserRepository repository; // Dependencia
+
+	public List<User> findAll() {
+
+		return repository.findAll();
+
+	}
+
+	public User findById(Long id) {
+		
+		Optional<User> obj  = repository.findById(id);
+		
+		return obj.get();
+	}
+
+}
